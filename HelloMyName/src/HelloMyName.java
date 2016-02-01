696dd1158354dd4ac55a82324d92a45fe1826704
Nasim Odilov <Nasim Odilov@NasimOdilov-PC>
2016-02-01 16:36:58 -0500

    HelloMyName

---
+++ HelloMyName/src/HelloMyName.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloMyName {
+      public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	  Scanner keyboard = new Scanner(System.in);
+      System.out.println("Hello " + keyboard.next());
+	}
+
+}
